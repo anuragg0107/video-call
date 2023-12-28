a3ac2d640f889dcdd5e500c868196f64edc56175
anurag gupta <101393446+anuragg0107@users.noreply.github.com>
2023-12-28 17:45:46 +0530

    update link

--- src/Component/Root/RoomPage.jsx
+++ src/Component/Root/RoomPage.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import {ZegoUIKitPrebuilt} from "@zegocloud/zego-uikit-prebuilt"
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 const RoomPage = () => {
-    const { roomId } = useParams()
+  const { roomId } = useParams();
 
-    const myMeeting = async(el) =>{
-        const appID = 851189778;
-        const serverSecret = "8daed6fbb98558149ce5af09f1c6a6fe";
-        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId,  Date.now().toString(),  "anurag");
+  const myMeeting = async (el) => {
+    const appID = 851189778;
+    const serverSecret = "8daed6fbb98558149ce5af09f1c6a6fe";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now().toString(),
+      "anurag"
+    );
 
-        const token = ZegoUIKitPrebuilt.create(kitToken)
-       
-        token.joinRoom({
-            container : el,
-            sharedLinks : [
-             {
-                name : "Copy Link",
-                url : `http://localhost:3000/room/${roomId}`
-             }
-            ],
-            scenario : {
-                mode :ZegoUIKitPrebuilt.OneONoneCall,
-            },
-            showScreenSharingButton: true
-        })
-    }
+    const token = ZegoUIKitPrebuilt.create(kitToken);
+
+    token.joinRoom({
+      container: el,
+      sharedLinks: [
+        {
+          name: "Copy Link",
+          url: `https://video-call-eosin-tau.vercel.app/room/${roomId}`,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+      showScreenSharingButton: true,
+    });
+  };
   return (
     <div>
-        <div ref={myMeeting} />
-         </div>
-  )
-}
+      <div ref={myMeeting} />
+    </div>
+  );
+};
 
-export default RoomPage
+export default RoomPage;
